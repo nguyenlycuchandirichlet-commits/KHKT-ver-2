212f8715cfbed225773cc5aa161cc78b35bb8032
nguyenlycuchandirichlet-commits <nguyenlycuchandirichlet@gmail.com>
2026-09-20 22:42:47 +0700

    Updated ollama.ts

--- src/lib/ollama.ts
+++ src/lib/ollama.ts
@@ -27,7 +27,7 @@ export type EvaluationResult = {
 };
 
 async function callOllama(promptText: string): Promise<string> {
- const response = await fetch('https://jake-jumping-bend-specially.trycloudflare.com /api/generate', {
+ const response = await fetch('https://checkout-peer-machinery-directions.trycloudflare.com ', {
     method: 'POST',
     mode: 'cors',
     headers: { 'Content-Type': 'application/json'},
